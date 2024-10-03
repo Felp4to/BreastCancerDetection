b7b4670bb1b72651621dbbf900c4553e09fcf23f
Felp4to <tardioli.paolo@gmail.com>
2024-10-03 12:10:12 +0200

    test preprocessing with clahe

--- preprocessing/py/clahe.py
+++ preprocessing/py/clahe.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 # return equalized image with clahe method
-def apply_equalization_clahe(path, histogram, clipLimit=2.0, tileGridSize=(8, 8)):
+def apply_equalization_clahe(path, histogram, show_result, clipLimit=2.0, tileGridSize=(8, 8)):
 
     # upload gray scale image
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
@@ -15,9 +15,11 @@ def apply_equalization_clahe(path, histogram, clipLimit=2.0, tileGridSize=(8, 8)
     # apply CLAHE to the image
     clahe_img = clahe.apply(img)
 
-    if(histogram == 1): 
+    if(histogram): 
         show_histogram(img, "original")
-        show_histogram(clahe, "clahe")
+        show_histogram(clahe_img, "clahe")
+    if(show_result):
+        show_difference(img, clahe_img)
 
 
 # show histogram
